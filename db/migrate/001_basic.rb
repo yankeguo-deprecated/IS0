ebdb77fa2494cdfb734b182f244d67e0f68cc4d5
YANKE Guo <me@yanke.io>
2015-02-01 07:02:42 +0800

    fix column char length for better performance

--- db/migrate/001_basic.rb
+++ db/migrate/001_basic.rb
@@ -1,9 +1,9 @@
 class Basic < ActiveRecord::Migration
   def change
     create_table :urls do |t|
-      t.text    :long_url,  null: false
-      t.string  :hash_md5,  null: false, index: true
-      t.string  :hash_sha1, null: false, index: true
+      t.text    :long_url,              null: false
+      t.column  :hash_md5,  "char(32)", null: false, index: true
+      t.column  :hash_sha1, "char(40)", null: false, index: true
       t.timestamps
     end
   end
